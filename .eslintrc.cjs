5df6305a6d895c21ddd57710597773514a21cb25
Meghiya Tulse <meghiyatulse@Meghiyas-MacBook-Air.local>
2026-06-21 15:29:02 +0530

    fix: update ESLint ignore patterns and refactor imports in useToast and transactions services

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -7,7 +7,7 @@ module.exports = {
     'plugin:react/jsx-runtime',    // disables 'React must be in scope' for new JSX transform
     'plugin:react-hooks/recommended',
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs', 'node_modules'],
+  ignorePatterns: ['dist', '.eslintrc.cjs', 'node_modules', 'hardhat.config.js', 'hardhat.config.cjs'],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   settings: { react: { version: '18.3' } },
   plugins: ['react-refresh'],
